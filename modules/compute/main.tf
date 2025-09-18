916a22844959172004ee1d0f55ad5d2038e0b0ae
adnantajir <adnantajir30@gmail.com>
2025-09-18 23:51:28 +0530

    Fix formatting of site_config block in Windows web app resource

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_windows_web_app" "web" {
   resource_group_name = var.resource_group_name
   location            = var.location
   service_plan_id     = azurerm_service_plan.web.id
-  site_config {
+  site_config  {
     always_on = var.always_on
   }
   app_settings              = var.app_settings
